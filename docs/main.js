76fa922cadfe82ff8b4dc4b89642764f4c7ce1b4
Chris Baiamonte <baiamonte.chris@gmail.com>
2021-01-20 23:20:03 -0500

    Setup basic UI and save/load code

--- docs/main.js
+++ docs/main.js
@@ -1,10 +1,42 @@
 "use strict";
 
+var characters,quests,completions;
+
 function initialize() {
-  alert("testing");
+  loadData();
+}
+
+function update() {
+  
 }
 
 
+function loadData() {
+  characters = JSON.parse(localStorage.getItem("characters")) || {};
+  quests = JSON.parse(localStorage.getItem("quests")) || {};
+  completions = JSON.parse(localStorage.getItem("completions")) || {};
+}
 
+function saveData() {
+  localStorage.setItem("characters",JSON.stringify(characters));
+  localStorage.setItem("quests",JSON.stringify(quests));
+  localStorage.setItem("completions",JSON.stringify(completions));
+}
+
+function addCharacter(character) {
+
+}
+
+function removeCharacter(character) {
+
+}
+
+function addQuest(quest) {
+
+}
+
+function addCompletion(character, quest) {
+
+}
 
 $(initialize);
